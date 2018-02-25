b1cc76b6c605b488240aaaec5b264c37784ccdce
BryceKopan <brycekopan@gmail.com>
2018-02-25 03:54:48 -0600

    Bug Fixes

--- Sources/TerraDrop-Server/Handlers.swift
+++ Sources/TerraDrop-Server/Handlers.swift
@@ -7,19 +7,31 @@ public class Handlers
         var drop = FullDrop() 
 
         drop.latitude = Double(request.param(name: "latitude", defaultValue: nil)!)!
+        print("lat")
         drop.longitude = Double(request.param(name: "longitude", defaultValue: nil)!)!
+        print("long")
         drop.userID = Int(request.param(name: "userID", defaultValue: nil)!)!
+        print("user")
         drop.title = String(request.param(name: "title", defaultValue: nil)!)!
+        print("title")
         drop.message = String(request.param(name: "message", defaultValue: nil)!)!
+        print("meesage")
 
-        let querySuccess = mysql.query(statement: "INSERT INTO TerraDrop (Hidden, Latitude, Longitude, Title, Message, UserID) VALUES (FALSE, \(drop.latitude), \(drop.longitude), \(drop.title), \(drop.message), \(drop.userID))")
+        let querySuccess = mysql.query(statement: "INSERT INTO TerraDrop (Hidden, Latitude, Longitude, Title, Message, UserID) VALUES (FALSE, \(drop.latitude), \(drop.longitude), '\(drop.title)', '\(drop.message)', \(drop.userID))")
 
         guard querySuccess else
         {
             print("[PostDrop] Query Failed")
+            
+            response.setHeader(.contentType, value: "text")
+            response.appendBody(string: "FALSE")
+            response.completed()
             return
-        }  
+        }
 
+        response.setHeader(.contentType, value: "text")
+        response.appendBody(string: "TRUE")
+        response.completed()
     }
 
     public static func getDrops(request: HTTPRequest, response: HTTPResponse)
@@ -103,5 +115,10 @@ public class Handlers
             print("[GetUsers] JSON Encoding failed")
             return
         }
+    }
+
+    private static func store<T>(sqlData: String, in array: [T], using closure: () -> [T])
+    {
+        
     }
 }
